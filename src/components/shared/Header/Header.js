23c0587781096bdd5ea6fd9a583e52a28641780e
Md. Shamrat Hossain <shamratpg@gmail.com>
2022-11-17 19:27:32 +0600

    first commit

---
+++ src/components/shared/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    const menu = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/'>About</Link></li>
+        <li><Link to='/'>Appointment</Link></li>
+        <li><Link to='/'>Reviews</Link></li>
+        <li><Link to='/'>Contact Us</Link></li>
+        <li><Link to='/'>Login</Link></li>
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <Link to='/' className="btn btn-ghost normal-case text-xl">Doctor's Portal</Link>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        {menu}
+                    </ul>
+                </div>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </label>
+                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                        {menu}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;

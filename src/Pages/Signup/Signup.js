041eb83312bc6cd4176bd20c15dc079096b215f6
Md. Shamrat Hossain <shamratpg@gmail.com>
2022-11-21 22:13:09 +0600

    added login & signup page

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Signup = () => {
     return (
         <div className='flex justify-center items-center my-16'>
             <form className="form-control shadow-lg p-6 rounded-lg">
-                <h1 className='text-center text-xl font-semibold'>Login</h1>
+                <h1 className='text-center text-xl font-semibold'>Signup</h1>
                 <div>
+
                     <label className="label">
                         <span className="label-text font-semibold">Your Email</span>
                     </label>
@@ -23,9 +24,9 @@ const Login = () => {
                         <Link className="label-text-alt">Forget Password?</Link>
                     </label>
                 </div>
-                <input className='btn btn-accent text-white' type='submit' value='logIn' />
+                <input className='btn btn-accent text-white' type='submit' value='Sign Up' />
                 <label className="label">
-                    <span className='label-text-alt'>New to Doctor's Portal? <Link to='/signup' className="text-secondary">Create new account</Link></span>
+                    <span className='label-text-alt'>Already have an account <Link to='/login' className="text-secondary">Log in</Link></span>
                 </label>
                 <div className="divider">OR</div>
                 <button className='btn btn-outline btn-accent' type='submit'>Continue with Google</button>
@@ -34,4 +35,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Signup;
--- src/Router/OpenRoutes.js
+++ src/Router/OpenRoutes.js
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import AppointmentPage from "../Pages/AppointmentPage/AppointmentPage";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
+import Signup from "../Pages/Signup/Signup";
 
 const router = createBrowserRouter([
     {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
             },
             {
                 path: '/appointment',
